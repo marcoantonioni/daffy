2c0b7a19ecf4f9d70777d4faa2da4107ce5f4f5c
Kyle Dawson <Kyle.Dawson@us.ibm.com>
2023-02-23 23:02:46 -0500

    Instana

---
+++ daffydoc/docs/js/instana.js
@@ -0,0 +1,11 @@
+<script>
+  (function(s,t,a,n){s[t]||(s[t]=a,n=s[a]=function(){n.q.push(arguments)},
+  n.q=[],n.v=2,n.l=1*new Date)})(window,"InstanaEumObject","ineum");
+  ineum('reportingUrl', 'https://instana.apps.lambda05.ibm01.techdemo-daffy.com/eum/');
+  ineum('key', 'wN3YIaCKTUmliqeZuKp4LQ');
+  ineum('trackSessions');
+</script>
+
+<script defer crossorigin="anonymous" src="https://instana.apps.lambda05.ibm01.techdemo-daffy.com/eum/eum.min.js">
+
+</script>
